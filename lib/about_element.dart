29d33809e0ef469a3673e53593b57a61f129732c
martin.haesler <martin.haesler@gmail.com>
2015-03-25 22:30:47 +0000

    add html for about element

--- lib/about_element.dart
+++ lib/about_element.dart
@@ -6,11 +6,12 @@ import 'package:polymer/polymer.dart';
 @CustomTag('about-element')
 
 class AboutElement extends PolymerElement {
+  String about="Martin Haesler is a former sales man for plumbing and heating projects, who after 17 years of working in this line of work, decided it was time for a change, see something of the world, and try for a completely different career path. Having always been interested in computer technology, an avid Linux user for many years, he decided on programming as a new career. Martin Haesler is quick to learn new things, self motivated and eager to improve.";
 
   /// Constructor used to create instance of AboutElement.
   AboutElement.created() : super.created() {
   }
-
+  
   /*
    * Optional lifecycle methods - uncomment if needed.
    *
