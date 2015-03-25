a22b104a00fe1b3900badce5da92166bac024d29
martin.haesler <martin.haesler@gmail.com>
2015-03-25 22:00:22 +0000

    add about-element

---
+++ lib/about_element.dart
@@ -0,0 +1,40 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer about-element element.
+ */
+@CustomTag('about-element')
+
+class AboutElement extends PolymerElement {
+
+  /// Constructor used to create instance of AboutElement.
+  AboutElement.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of about-element is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of about-element is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// about-element is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when about-element has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
