4dbaca4b63a1b56b04e6d40359358648387cf3e8
martin.haesler <martin.haesler@gmail.com>
2015-03-26 13:58:57 +0000

    add work-experience html + dart

---
+++ lib/work_experience.dart
@@ -0,0 +1,40 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer work-experience element.
+ */
+@CustomTag('work-experience')
+
+class WorkExperience extends PolymerElement {
+
+  /// Constructor used to create instance of WorkExperience.
+  WorkExperience.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of work-experience is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of work-experience is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// work-experience is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when work-experience has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
