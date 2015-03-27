36d2691b3fe014944399965851c20a24694127d7
martin.haesler <martin.haesler@gmail.com>
2015-03-27 14:09:21 +0000

    generate awards_element

---
+++ lib/awards_element.dart
@@ -0,0 +1,40 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer awards-element element.
+ */
+@CustomTag('awards-element')
+
+class AwardsElement extends PolymerElement {
+
+  /// Constructor used to create instance of AwardsElement.
+  AwardsElement.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of awards-element is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of awards-element is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// awards-element is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when awards-element has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
