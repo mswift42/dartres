37f363b8e54bc474027f6a38ece8202a7599786f
martin.haesler <martin.haesler@gmail.com>
2015-03-27 13:20:47 +0000

    add edu_cation element

---
+++ lib/edu_cation.dart
@@ -0,0 +1,40 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer edu-cation element.
+ */
+@CustomTag('edu-cation')
+
+class EduCation extends PolymerElement {
+
+  /// Constructor used to create instance of EduCation.
+  EduCation.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of edu-cation is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of edu-cation is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// edu-cation is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when edu-cation has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
