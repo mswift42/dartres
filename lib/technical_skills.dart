72610e8e79012f58409f0552c4bf33b5f1fb0a4e
martin.haesler <martin.haesler@gmail.com>
2015-03-27 14:09:33 +0000

    generate technical-skills element

---
+++ lib/technical_skills.dart
@@ -0,0 +1,40 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer technical-skills element.
+ */
+@CustomTag('technical-skills')
+
+class TechnicalSkills extends PolymerElement {
+
+  /// Constructor used to create instance of TechnicalSkills.
+  TechnicalSkills.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of technical-skills is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of technical-skills is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// technical-skills is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when technical-skills has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
