05dedf4a17ce3866efbe9d59a8b7cdbc6a121b1e
martin.haesler <martin.haesler@gmail.com>
2015-03-28 20:18:57 +0000

    add projectimg

--- lib/project_model.dart
+++ lib/project_model.dart
@@ -5,7 +5,7 @@ import 'package:polymer/polymer.dart';
 class SingleProject extends Observable {
   @observable String projecttitle;
   @observable String githuburl;
-  @observable String shortdescription;
+  @observxoable String shortdescription;
   @observable String titleimg;
 
   SingleProject(
@@ -15,5 +15,11 @@ class SingleProject extends Observable {
 List<SingleProject> projects = [
   new SingleProject("MetalHead", "https://github.com/mswift42/MetalHead",
       "MetalHead is an old-school text adventure written in Common Lisp for the 2013 Lisp in Summer Projects competition.",
-      "../images/metalhead/mh1.png")
+      "../images/metalhead/mh1.png"),
+  new SingleProject("Theme Creator", "https://github.com/mswift42/theme-creator",
+      "Emacs Theme Creator is a lets you quickly create themes for Emacs.",
+      "../images/themecreator/tc1.png"),
+  new SingleProject("Beebster", "https://github.com/mswift42/go-beebster",
+      "Beebset is a Web Gui for the command line app get_iplayer.",
+      "../images/beebster/beeb1.png")
 ];
