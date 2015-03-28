1cc119dc68a6e77166d57b7ee658b9a359f0a84a
martin.haesler <martin.haesler@gmail.com>
2015-03-28 11:38:17 +0000

    generate personal-projects element

---
+++ lib/single_project.dart
@@ -0,0 +1,40 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer single-project element.
+ */
+@CustomTag('single-project')
+
+class SingleProject extends PolymerElement {
+
+  /// Constructor used to create instance of SingleProject.
+  SingleProject.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of single-project is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of single-project is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// single-project is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when single-project has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
