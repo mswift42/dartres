f35558778bbf77575a72c1265a5c64fb2aa83e10
martin.haesler <martin.haesler@gmail.com>
2015-03-29 17:41:51 +0100

    add second mh projectdetail

--- lib/projectinfomodel.dart
+++ lib/projectinfomodel.dart
@@ -17,7 +17,7 @@ class ProjectInfo extends Observable {
   ProjectInfo(this.projecttitle, this.projectdetails);
 }
 
-list<ProjectInfo> metalheadprojectinfo = [
+List<ProjectInfo> metalheadprojectinfo = [
   new ProjectInfo("MetalHead",[
     new ProjectDetail("../images/metalhead/metalheadworld1.png", 
         "Classes for rooms, items and player. fdescription is for inital description of a location or item, ldescriptin describes a location which has been visited before. uexit is an unconditinal exit, i.e. can be visited always. nexit is not an exit, but instead of printing 'no exit' it prints a more interesting text. cexit is a conditional exit, i.e, for locations you need a key, or have accomplished some task previously. flags are for storing information about a location, i.e. has this location been visited before. things is a list of items in a location. the organisation of these classes has been heavily influenced by a pdf called learning ZIL (http://www.xlisp.org/zil.pdf)"),
