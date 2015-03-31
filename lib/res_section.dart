00abb3ff2e1c021068f74434262c01ccadac4a54
martin.haesler <martin.haesler@gmail.com>
2015-03-31 21:09:49 +0100

    generate section_header element

---
+++ lib/res_section.dart
@@ -0,0 +1,40 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer res-section element.
+ */
+@CustomTag('res-section')
+
+class ResSection extends PolymerElement {
+
+  /// Constructor used to create instance of ResSection.
+  ResSection.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of res-section is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of res-section is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// res-section is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when res-section has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
