ace8edcb854da2494bc4e708cc4abf148792624b
martin.haesler <martin.haesler@gmail.com>
2015-03-31 21:10:04 +0100

    generate res-section element

---
+++ lib/section_header.dart
@@ -0,0 +1,40 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer section-header element.
+ */
+@CustomTag('section-header')
+
+class SectionHeader extends PolymerElement {
+
+  /// Constructor used to create instance of SectionHeader.
+  SectionHeader.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of section-header is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of section-header is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// section-header is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when section-header has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
