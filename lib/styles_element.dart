27b462bb1b5bfb936b2ee8e2609b840042ab8b01
martin.haesler <martin.haesler@gmail.com>
2015-03-31 20:49:18 +0100

    generate styles_element

---
+++ lib/styles_element.dart
@@ -0,0 +1,40 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer styles-element element.
+ */
+@CustomTag('styles-element')
+
+class StylesElement extends PolymerElement {
+
+  /// Constructor used to create instance of StylesElement.
+  StylesElement.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of styles-element is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of styles-element is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// styles-element is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when styles-element has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
