5753d0d7d01ff917f9c9235056e97e9b02577982
martin.haesler <martin.haesler@gmail.com>
2015-04-04 20:57:08 +0100

    generate project_summary element

---
+++ lib/project_summary.dart
@@ -0,0 +1,40 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer project-summary element.
+ */
+@CustomTag('project-summary')
+
+class ProjectSummary extends PolymerElement {
+
+  /// Constructor used to create instance of ProjectSummary.
+  ProjectSummary.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of project-summary is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of project-summary is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// project-summary is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when project-summary has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
